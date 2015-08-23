b98f716321771155af9f37e182b964ea50ea8d98
Dennis Pitallano <dennis.pitallano@hotmail.co.uk>
2015-08-23 16:44:07 +0800

    Add Bin Model in IM.Models Project

---
+++ IM/IM.Models/Bin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BLToolkit.DataAccess;
+using BLToolkit.Mapping;
+
+namespace IM.Models
+{
+    [TableName("ref_bin")]
+    public class Bin
+    {
+        [PrimaryKey,NonUpdatable]
+        [MapField("bin_id")]
+        public int Id { get; set; }
+        [MapField("bin_code")]
+        public string BinCode { get; set; }
+        [MapField("bin_desc")]
+        public string BinDescription { get; set; }
+        [MapField("dep_id")]
+        public int DepartmentId { get; set; }
+        [MapField("uid")]
+        public Guid Uid { get; set; }
+    }
+}
