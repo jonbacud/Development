d86df8546d581e9aea60e1c96eb84e9a91a943bc
Dennis Pitallano <dennis.pitallano@hotmail.co.uk>
2015-08-23 16:40:44 +0800

    Implement Bin Manager

---
+++ IM/IM.BusinessLogic/DataManager/BinManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IM.BusinessLogic.DataAccess;
+using IM.Models;
+
+namespace IM.BusinessLogic.DataManager
+{
+    public class BinManager :IBaseManager<Bin>
+    {
+        public int Identity
+        {
+            get; set;
+        }
+
+        public void Save(Bin bin)
+        {
+            if (bin.Id>0)
+            {
+                Accessor.Query.Update(bin);
+            }
+            else
+            {
+                Accessor.Query.Insert(bin);
+            }
+        }
+
+        public void Save(List<Bin> bins)
+        {
+            foreach (var bin in bins)
+            {
+                Save(bin);
+            }
+        }
+
+        public void Delete(Bin bin)
+        {
+            Accessor.Query.Delete(bin);
+        }
+
+        public void Delete(List<Bin> bins)
+        {
+            foreach (var bin in bins)
+            {
+                Delete(bin);
+            }
+        }
+
+        public List<Bin> FetchAll()
+        {
+            return Accessor.Query.SelectAll<Bin>() ?? new List<Bin>();
+        }
+
+        public Bin FetchById(int key)
+        {
+            return Accessor.Query.SelectByKey<Bin>(key) ?? new Bin();
+        }
+
+        #region Accessor
+        BinAccessor Accessor
+        {
+            [System.Diagnostics.DebuggerStepThrough]
+            get { return BinAccessor.CreateInstance(); }
+        }
+        #endregion
+    }
+}
