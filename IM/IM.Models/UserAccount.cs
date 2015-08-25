f9474b8b9807dfc5f3eed7f1642d626ef730399c
Dennis Pitallano <dennis.pitallano@hotmail.co.uk>
2015-08-25 23:43:49 +0800

    Add User Account

---
+++ IM/IM.Models/UserAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BLToolkit.DataAccess;
+using BLToolkit.Mapping;
+
+namespace IM.Models
+{
+    [TableName("ref_user")]
+    public   class UserAccount
+    {
+
+        [MapField("userid")]
+        public int UserId { get; set; }
+
+        [MapField("username")]
+        public string UserName { get; set; }
+
+        [MapField("password")]
+        public string Password { get; set; }
+
+        [MapField("position")]
+        public string Position { get; set; }
+
+        [MapField("department_id")]
+        public int DeaprtmentId { get; set; }
+
+        [MapField("firstname")]
+        public string FishName { get; set; }
+
+        [MapField("lastname")]
+        public string LastName { get; set; }
+
+        [MapField("")]
+        public bool IsActive { get; set; }
+    }
+}
