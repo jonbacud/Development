cb6656d562b7584333d1a9ed4a45e47031bd6c92
Dennis Pitallano <dennis.pitallano@hotmail.co.uk>
2015-08-31 09:10:30 +0800

    Add Method to get last Reference Number
    Update for Auto generated Reference number based on the last
    transaction

--- IM/IM.BusinessLogic/DataAccess/RequisitionAccessor.cs
+++ IM/IM.BusinessLogic/DataAccess/RequisitionAccessor.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using BLToolkit.Data;
+using BLToolkit.DataAccess;
 
 namespace IM.BusinessLogic.DataAccess
 {
     public abstract class RequisitionAccessor : AccessorBase<RequisitionAccessor.DB, RequisitionAccessor>
     {
         public class DB : DbManager { public DB() : base("IMConnectionString") { } }
+
+        [SqlQuery(@"Select top 1 reference_number from trn_requisition order by ris_id desc")]
+        public abstract string GetLastReferenceNumber();
     }
 }
--- IM/IM.BusinessLogic/DataManager/RequisitionManager.cs
+++ IM/IM.BusinessLogic/DataManager/RequisitionManager.cs
@@ -59,6 +59,16 @@ namespace IM.BusinessLogic.DataManager
             return Accessor.Query.SelectByKey<Requisition>(key);
         }
 
+        private string LastReferenceNumber
+        {
+            get { return Accessor.GetLastReferenceNumber(); }
+        }
+
+        public int ReferenceNumber
+        {
+            get { return string.IsNullOrEmpty(LastReferenceNumber) ? 10000 : int.Parse(LastReferenceNumber.Split('-')[1]);}
+        }
+
         #region Accessor
         RequisitionAccessor Accessor
         {
